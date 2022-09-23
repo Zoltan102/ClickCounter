f842dfacb64055c95be2bdc40804a15730d668f5
Zoltan102 <suveges.zoltan@petrik.hu>
2022-09-23 08:25:26 +0200

    kinézet

--- app/src/main/java/com/example/clickcounter/MainActivity.java
+++ app/src/main/java/com/example/clickcounter/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.clickcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button plus;
+    Button minus;
+    TextView counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void init(){
     }
 }
